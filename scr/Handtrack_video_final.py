8a1d98bedbb69c208818786adbb4cdd39a69c4e0
momar82 <m.omar82@gmail.com>
2024-04-11 04:05:51 +0300

    edit

--- scr/Handtrack_video_final.py
+++ scr/Handtrack_video_final.py
@@ -4,6 +4,16 @@ import pytesseract
 import time
 from collections import deque
 from statistics import mode, StatisticsError
+import os
+
+
+file_path = "scr/Dataset/vedio_3.mp4"
+
+if os.path.exists(file_path):
+    print(f"The file '{file_path}' exists.")
+else:
+    print(f"The file '{file_path}' does not exist.")
+
 
 # Initialize variables for text smoothing and handling
 previous_text = ""
@@ -15,7 +25,7 @@ hands = mp_hands.Hands(min_detection_confidence=0.7, min_tracking_confidence=0.5
 mp_drawing = mp.solutions.drawing_utils
 
 # Load the video
-video_cap = cv2.VideoCapture("./Dataset/vedio_3.mp4")
+video_cap = cv2.VideoCapture(file_path)
 
 while True:
     ret, frame = video_cap.read()
